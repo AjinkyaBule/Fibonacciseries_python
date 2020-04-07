48a9256ce6b5567439c773f0a32e71e7146204b8
Ajinkya Bule <54928836+AjinkyaBule@users.noreply.github.com>
2020-04-07 16:43:53 +0530

    Add files via upload

---
+++ fibonacciseries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#CODE: TO PRINT FIBONACCI SERIES.
+n=int(input("Enter the total number of terms you want in series:"));
+if n<0:
+    print("enter positive value only");
+elif n==1:
+    print(1);
+else:
+    print("Fibonacci series consisting",n,"terms is:");
+    a=0;
+    b=1;
+    for i in range(0,n):
+        print(a);
+        c=a+b;
+        a=b;
+        b=c;
+
+    
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
